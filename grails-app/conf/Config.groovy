197612d29dfdffddebc08b54e33da6675f3e3e09
J. David Beutel <git@getsu.com>
2014-01-25 17:04:36 -1000

    added spring-security-core and ran s2-quickstart

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -113,3 +113,19 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.getsu.wcy.auth.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.getsu.wcy.auth.UserRole'
+grails.plugin.springsecurity.authority.className = 'com.getsu.wcy.auth.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                              ['permitAll'],
+	'/index':                         ['permitAll'],
+	'/index.gsp':                     ['permitAll'],
+	'/**/js/**':                      ['permitAll'],
+	'/**/css/**':                     ['permitAll'],
+	'/**/images/**':                  ['permitAll'],
+	'/**/favicon.ico':                ['permitAll']
+]
+
