fab5bbec80a067c4974a070512ca931bb299a987
J. David Beutel <jbeutel@hawaii.edu>
2014-02-26 16:26:04 -1000

    upgrade to Grails 2.3.6

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -55,7 +55,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.50"
+        build ":tomcat:7.0.50.1"
 
         // plugins for the compile step
         compile ":scaffolding:2.0.1"
@@ -69,7 +69,7 @@ grails.project.dependency.resolution = {
         compile ':spring-security-oauth-google:0.2'
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.7" // or ":hibernate4:4.1.11.6"
+        runtime ":hibernate:3.6.10.8" // or ":hibernate4:4.1.11.6"
         runtime ":database-migration:1.3.8"
         runtime ":jquery:1.10.2.2"
         runtime ":resources:1.2.1"
